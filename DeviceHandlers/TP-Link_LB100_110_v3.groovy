6697472ce38a30e6e08ac77269da0c6a7d13b433
DaveGut <djgutheinz@outlook.com>
2017-07-04 17:08:38 -0500

    Add files via upload

--- DeviceHandlers/TP-Link_LB100_110_v3.groovy
+++ DeviceHandlers/TP-Link_LB100_110_v3.groovy
@@ -1,129 +1,138 @@
 /*
 TP-Link LB100 and LB110 Device Handler
-FOR USE ONLY WITH 'TP-LinkServer_v3.js'
+FOR USE ONLY WITH 'TP-LinkServer_v4.js'
 
 Copyright 2017 Dave Gutheinz
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
-file except in compliance with the License. You may obtain a copy of the License at:
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:
 
-		http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
         
-Unless required by applicable law or agreed to in writing, software distributed under 
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
-ANY KIND, either express or implied. See the License for the specific language governing 
-permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-Supported models and functions:  This device supports the TP-Link LB100 and LB120.
-
-Notes: 
-1.	This Device Handler requires an operating Windows 10 PC Bridge running version 3.0 of
-	'TP-LinkServer.js'.  It is NOT fully compatible with earlier versions.
-2.	This device supports the TP-Link LB100 and LB110 devices.  It supports the on/off and
-	bightness functions.
+Supported models and functions:  This device supports the TP-Link LB100 and LB110 bulbs without Energy Meter functions.
 
 Update History
-	06/01/2017	- Initial release of Version 3.0.  Following add-ons from version 2.3
-				  1.  Compatible with SmartTiles / ActionTiles
-                  2.  Added error messaging to final version.
-                  3.  Added error messages to appear in ST phone app, Recently tab.
-                  4.  Modified coloring and added a Waiting state that displays whenever
-                  	  a function is actuated to indicate waiting for response from the
-                      Bridge.
+07-04-2017    - Updated to common format and interface with Emeter Version
 */
 
 metadata {
-	definition (
-    	name: "TP-Link LB100 and 110", namespace: "djg", author: "Dave Gutheinz") {
-		capability "Switch"
-		capability "Switch Level"
-		capability "refresh"
+    definition (
+        name: "TP-Link LB110 and LB100", namespace: "djg", author: "Dave Gutheinz") {
+        capability "switch"
+        capability "Switch Level"
+        capability "refresh"
         capability "Sensor"
-		capability "Actuator"
-	}
-	tiles(scale: 2) {
-		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc",
-				nextState:"waiting"
-				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff",
-				nextState:"waiting"
-				attributeState "waiting", label:'${name}', action:"switch.on", icon:"st.switches.light.on", backgroundColor:"#15EE10",
-				nextState:"on"
+        capability "Actuator"
+    }
+    tiles(scale: 2) {
+        multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+                attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc",
+                nextState:"waiting"
+                attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff",
+                nextState:"waiting"
+                attributeState "waiting", label:'${name}', action:"switch.on", icon:"st.switches.light.on", backgroundColor:"#15EE10",
+                nextState:"on"
                 attributeState "offline", label:'Comms Error', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#e86d13",
                 nextState:"waiting"
-			}
-			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
-				attributeState "level", label: "Brightness: ${currentValue}", action:"switch level.setLevel"
-			}
-		}
-		standardTile("refresh", "capability.refresh", width: 2, height: 2,  decoration: "flat") {
-			state ("default", label:"Refresh", action:"refresh.refresh", icon:"st.secondary.refresh", backgroundColor:"#ffffff")
-		}         
-		main("switch")
-		details(["switch", "refresh"])
+            }
+            tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+                attributeState "level", label: "Brightness: ${currentValue}", action:"switch level.setLevel"
+            }
+        }
+        standardTile("refresh", "capability.refresh", width: 2, height: 1,  decoration: "flat") {
+            state ("default", label:"Refresh", action:"refresh.refresh", backgroundColor:"#ffffff")
+        }         
+        main("switch")
+        details("switch", "colorTempSliderControl", "colorTemp", "power", "weekTotal", "monthTotal", "engrToday", "weekAverage", "monthAverage", "bulbMode", "refresh" ,"refreshStats")
     }
 }
+
 preferences {
-	input("deviceIP", "text", title: "Device IP", required: true, displayDuringSetup: true)
-	input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
+    input("deviceIP", "text", title: "Device IP", required: true, displayDuringSetup: true)
+    input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
 }
+
+def installed() {
+    updated()
+}
+
+def updated() {
+    unschedule()
+    runEvery15Minutes(refresh)
+    runIn(2, refresh)
+}
+
+//    ----- BASIC BULB COMMANDS ------------------------------------
 def on() {
-	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}', "commandResponse")
+    sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}', "commandResponse")
 }
 def off() {
-	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}', "commandResponse")
+    sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}', "commandResponse")
 }
+
 def setLevel(percentage) {
-	percentage = percentage as int
+    percentage = percentage as int
     sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"brightness":${percentage}}}}""", "commandResponse")
 }
-def refresh(){
-	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "refreshResponse")
-}
-private sendCmdtoServer(command, action){
-	sendEvent(name: "switch", value: "waiting", isStateChange: true)
-	def headers = [:] 
-	headers.put("HOST", "$gatewayIP:8082")   // port 8082 must be same as value in TP-LInkServerLite.js
-	headers.put("tplink-iot-ip", deviceIP)
-    headers.put("tplink-command", command)
-	headers.put("command", "deviceCommand")
-	sendHubCommand(new physicalgraph.device.HubAction([
-		headers: headers],
-		device.deviceNetworkId,
-		[callback: action]
-	))
-}
+
 def commandResponse(response){
-	if (response.headers["cmd-response"] == "TcpTimeout") {
-		log.error "$device.name $device.label: Communications Error"
-		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod commandResponse", isStateChange: true)
-     } else {
-		def cmdResponse = parseJson(response.headers["cmd-response"])
-		state =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
-		parseStatus(state)
-	}
+    if (response.headers["cmd-response"] == "TcpTimeout") {
+        log.error "$device.name $device.label: Communications Error"
+        sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod commandResponse", isStateChange: true)
+    } else {
+        def cmdResponse = parseJson(response.headers["cmd-response"])
+        state =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
+        parseStatus(state)
+    }
 }
+
+//    ----- REFRESH ------------------------------------------------
+def refresh(){
+    sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "refreshResponse")
+}
+
 def refreshResponse(response){
-	if (response.headers["cmd-response"] == "TcpTimeout") {
-		log.error "$device.name $device.label: Communications Error"
-		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod refreshResponse", isStateChange: true)
+    if (response.headers["cmd-response"] == "TcpTimeout") {
+        log.error "$device.name $device.label: Communications Error"
+        sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod refreshResponse", isStateChange: true)
      } else {
-		def cmdResponse = parseJson(response.headers["cmd-response"])
-		state = cmdResponse.system.get_sysinfo.light_state
-		parseStatus(state)
-	}
+        def cmdResponse = parseJson(response.headers["cmd-response"])
+        state = cmdResponse.system.get_sysinfo.light_state
+        parseStatus(state)
+    }
 }
+
+//    ----- Parse State from Bulb Responses ------------------------
 def parseStatus(state){
-	def status = state.on_off
-	if (status == 1) {
-		status = "on"
-	} else {
-		status = "off"
-		state = state.dft_on_state
-	}
-	def level = state.brightness
-	log.info "$device.name $device.label: Power: ${status} / Brightness: ${level}%"
-	sendEvent(name: "switch", value: status, isStateChange: true)
-	sendEvent(name: "level", value: level, isStateChange: true)
+    def status = state.on_off
+    if (status == 1) {
+        status = "on"
+    } else {
+        status = "off"
+        state = state.dft_on_state
+    }
+    def mode = state.mode
+    def level = state.brightness
+    def color_temp = state.color_temp
+    log.info "$device.name $device.label: Power: ${status} / Brightness: ${level}%"
+    sendEvent(name: "switch", value: status, isStateChange: true)
+    sendEvent(name: "bulbMode", value: mode, isStateChange: true)
+    sendEvent(name: "level", value: level, isStateChange: true)
+    sendEvent(name: "colorTemperature", value: color_temp, isStateChange: true)
+}
+
+//    ----- Send the Command to the Bridge -------------------------
+private sendCmdtoServer(command, action){
+    def headers = [:] 
+    headers.put("HOST", "$gatewayIP:8082")   // Same as on Hub.
+    headers.put("tplink-iot-ip", deviceIP)
+    headers.put("tplink-command", command)
+    headers.put("command", "deviceCommand")
+    sendHubCommand(new physicalgraph.device.HubAction([
+        headers: headers],
+        device.deviceNetworkId,
+        [callback: action]
+    ))
 }
--- DeviceHandlers/TP-Link_LB130_v3.groovy
+++ DeviceHandlers/TP-Link_LB130_v3.groovy
@@ -4,168 +4,178 @@ FOR USE ONLY WITH 'TP-LinkServer_v3.js'
 
 Copyright 2017 Dave Gutheinz
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
-file except in compliance with the License. You may obtain a copy of the License at:
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:
 
-		http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
         
-Unless required by applicable law or agreed to in writing, software distributed under 
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
-ANY KIND, either express or implied. See the License for the specific language governing 
-permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-Supported models and functions:  This device supports the TP-Link LB130 device.
-
-Notes: 
-1.	This Device Handler requires an operating Windows 10 PC Bridge running version 3.0 of
-	'TP-LinkServer.js'.  It is NOT fully compatible with earlier versions.
-2.	This device supports the TP-Link LB100 and LB110 devices.  It supports the on/off,
-	bightness color temperature, Circadian (mode) and color functions.
+Supported models and functions:  This device supports the TP-Link LB130 without Energy Monitor Functions.
 
 Update History
-	06/01/2017	- Initial release of Version 3.0.  Following add-ons from version 2.3
-				  1.  Compatible with SmartTiles / ActionTiles
-                  2.  Added error messaging to final version.
-                  3.  Added error messages to appear in ST phone app, Recently tab.
-                  4.  Modified coloring and added a Waiting state that displays whenever
-                  	  a function is actuated to indicate waiting for response from the
-                      Bridge.
+07-04-2017    - Initial release of this Emeter Version
 */
 
 metadata {
-	definition (name: "TP-Link LB130", namespace: "djg", author: "Dave Gutheinz") {
-		capability "Switch"
-		capability "Switch Level"
-		capability "Color Control"
-		capability "Color Temperature"
-		capability "refresh"
-		capability "Sensor"
-		capability "Actuator"
-		attribute "bulbMode", "string"
-		command "setModeNormal"
-		command "setModeCircadian"
-	}
-	tiles(scale:2) {
-		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc",
-				nextState:"waiting"
-				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff",
-				nextState:"waiting"
-				attributeState "waiting", label:'${name}', action:"switch.on", icon:"st.switches.light.on", backgroundColor:"#15EE10",
-				nextState:"on"
+    definition (name: "TP-Link LB130", namespace: "djg", author: "Dave Gutheinz") {
+        capability "Switch"
+        capability "Switch Level"
+        capability "Color Control"
+        capability "Color Temperature"
+        capability "refresh"
+        capability "Sensor"
+        capability "Actuator"
+        attribute "bulbMode", "string"
+        command "setModeNormal"
+        command "setModeCircadian"
+    }
+    tiles(scale:2) {
+        multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+                attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc",
+                nextState:"waiting"
+                attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff",
+                nextState:"waiting"
+                attributeState "waiting", label:'${name}', action:"switch.on", icon:"st.switches.light.on", backgroundColor:"#15EE10",
+                nextState:"on"
                 attributeState "offline", label:'Comms Error', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#e86d13",
                 nextState:"waiting"
-			}
-			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
-				attributeState "level", label: "Brightness: ${currentValue}", action:"switch level.setLevel"
-			}
-			tileAttribute ("device.color", key: "COLOR_CONTROL") {
-        		attributeState "color", action:"setColor"
-			}
-		}
-		controlTile("colorTempSliderControl", "device.colorTemperature", "slider", width: 4, height: 1, inactiveLabel: false,
-		range:"(2500..9000)") {
-        	state "colorTemperature", action:"color temperature.setColorTemperature"
-		}
-		valueTile("colorTemp", "device.colorTemperature", inactiveLabel: false, decoration: "flat", height: 1, width: 2) {
-			state "colorTemp", label: '${currentValue}K'
-		}
-		standardTile("bulbMode", "bulbMode", width: 3, height: 2, decoration: "flat") {
-			state "normal", label:'Normal Mode', action:"setModeCircadian", backgroundColor:"#ffffff", nextState: "circadian"
-			state "circadian", label:'Circadian Mode', action:"setModeNormal", backgroundColor:"#00a0dc", nextState: "normal"
-		}
-		standardTile("refresh", "capability.refresh", width: 3, height: 2,  decoration: "flat") {
-			state ("default", label:"Refresh", action:"refresh.refresh", icon:"st.secondary.refresh", backgroundColor:"#ffffff")
-		}         
-		main("switch")
-		details(["switch", "colorTempSliderControl", "colorTemp", "bulbMode", "refresh"])
+            }
+            tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+                attributeState "level", label: "Brightness: ${currentValue}", action:"switch level.setLevel"
+            }
+            tileAttribute ("device.color", key: "COLOR_CONTROL") {
+                attributeState "color", action:"setColor"
+            }
+        }
+        controlTile("colorTempSliderControl", "device.colorTemperature", "slider", width: 4, height: 1, inactiveLabel: false,
+        range:"(2500..9000)") {
+            state "colorTemperature", action:"color temperature.setColorTemperature"
+        }
+        valueTile("colorTemp", "device.colorTemperature", inactiveLabel: false, decoration: "flat", height: 1, width: 2) {
+            state "colorTemp", label: '${currentValue}K'
+        }
+        standardTile("bulbMode", "bulbMode", width: 2, height: 1, decoration: "flat") {
+            state "normal", label:'Normal Mode', action:"setModeCircadian", backgroundColor:"#ffffff", nextState: "circadian"
+            state "circadian", label:'Circadian Mode', action:"setModeNormal", backgroundColor:"#00a0dc", nextState: "normal"
+        }
+        standardTile("refresh", "capability.refresh", width: 2, height: 1,  decoration: "flat") {
+            state ("default", label:"Refresh", action:"refresh.refresh", backgroundColor:"#ffffff")
+        }         
+        main("switch")
+        details("switch", "colorTempSliderControl", "colorTemp", "power", "weekTotal", "monthTotal", "engrToday", "weekAverage", "monthAverage", "bulbMode", "refresh" ,"refreshStats")
     }
 }
+
 preferences {
-	input("deviceIP", "text", title: "Device IP", required: true, displayDuringSetup: true)
-	input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
+    input("deviceIP", "text", title: "Device IP", required: true, displayDuringSetup: true)
+    input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
 }
+
+def installed() {
+    updated()
+}
+
+def updated() {
+    unschedule()
+    runEvery15Minutes(refresh)
+    runIn(2, refresh)
+}
+
+//    ----- BASIC BULB COMMANDS ------------------------------------
 def on() {
-	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}', "commandResponse")
+    sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}', "commandResponse")
 }
+
 def off() {
-	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}', "commandResponse")
+    sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}', "commandResponse")
 }
+
 def setLevel(percentage) {
-	percentage = percentage as int
-	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"brightness":${percentage}}}}""", "commandResponse")
+    percentage = percentage as int
+    sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"brightness":${percentage}}}}""", "commandResponse")
 }
+
 def setColorTemperature(kelvin) {
-	kelvin = kelvin as int
-	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"color_temp": ${kelvin},"hue":0,"saturation":0}}}""", "commandResponse")
+    kelvin = kelvin as int
+    sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"color_temp": ${kelvin},"hue":0,"saturation":0}}}""", "commandResponse")
 }
+
 def setModeNormal() {
-	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"normal"}}}""", "commandResponse")
+    sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"normal"}}}""", "commandResponse")
 }
+
 def setModeCircadian() {
-	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"circadian"}}}""", "commandResponse")
+    sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"circadian"}}}""", "commandResponse")
 }
+
 def setColor(Map color) {
-	def hue = color.hue * 3.6 as int
-	def saturation = color.saturation as int
-	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"color_temp":0,"hue":${hue},"saturation":${saturation}}}}""", "commandResponse")
+    def hue = color.hue * 3.6 as int
+    def saturation = color.saturation as int
+    sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"color_temp":0,"hue":${hue},"saturation":${saturation}}}}""", "commandResponse")
 }
-def refresh(){
-	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "refreshResponse")
-}
-private sendCmdtoServer(command, action){
-	def headers = [:] 
-	sendEvent(name: "switch", value: "waiting", isStateChange: true)
-	headers.put("HOST", "$gatewayIP:8082")   // port 8082 must be same as value in TP-LInkServerLite.js
-	headers.put("tplink-iot-ip", deviceIP)
-    headers.put("tplink-command", command)
-	headers.put("command", "deviceCommand")
-	sendHubCommand(new physicalgraph.device.HubAction([
-		headers: headers],
-		device.deviceNetworkId,
-		[callback: action]
-	))
-}
+
 def commandResponse(response){
-	if (response.headers["cmd-response"] == "TcpTimeout") {
-		log.error "$device.name $device.label: Communications Error"
-		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod commandResponse", isStateChange: true)
-     } else {
-		def cmdResponse = parseJson(response.headers["cmd-response"])
-		state =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
-		parseStatus(state)
-	}
+    if (response.headers["cmd-response"] == "TcpTimeout") {
+        log.error "$device.name $device.label: Communications Error"
+        sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod commandResponse", isStateChange: true)
+    } else {
+        def cmdResponse = parseJson(response.headers["cmd-response"])
+        state =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
+        parseStatus(state)
+    }
 }
+
+//    ----- REFRESH ------------------------------------------------
+def refresh(){
+    sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "refreshResponse")
+}
+
 def refreshResponse(response){
-	if (response.headers["cmd-response"] == "TcpTimeout") {
-		log.error "$device.name $device.label: Communications Error"
-		sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod refreshResponse", isStateChange: true)
+    if (response.headers["cmd-response"] == "TcpTimeout") {
+        log.error "$device.name $device.label: Communications Error"
+        sendEvent(name: "switch", value: "offline", descriptionText: "ERROR - OffLine - mod refreshResponse", isStateChange: true)
      } else {
-		def cmdResponse = parseJson(response.headers["cmd-response"])
-		state = cmdResponse.system.get_sysinfo.light_state
-		parseStatus(state)
-	}
+        def cmdResponse = parseJson(response.headers["cmd-response"])
+        state = cmdResponse.system.get_sysinfo.light_state
+        parseStatus(state)
+    }
 }
+
+//    ----- Parse State from Bulb Responses ------------------------
 def parseStatus(state){
-	def status = state.on_off
-	if (status == 1) {
-		status = "on"
-	} else {
-		status = "off"
-		state = state.dft_on_state
-	}
-	def mode = state.mode
-	def level = state.brightness
-	def color_temp = state.color_temp
-	def hue = state.hue
-	def saturation = state.saturation
-	log.info "$device.name $device.label: Power: ${status} / Mode: ${mode} / Brightness: ${level}% / Color Temp: ${color_temp}K / Hue: ${hue} / Saturation: ${saturation}"
-	sendEvent(name: "switch", value: status, isStateChange: true)
-	sendEvent(name: "bulbMode", value: mode, isStateChange: true)
-	sendEvent(name: "level", value: level, isStateChange: true)
-	sendEvent(name: "colorTemperature", value: color_temp, isStateChange: true)
-	sendEvent(name: "hue", value: hue, isStateChange: true)
-	sendEvent(name: "saturation", value: saturation, isStateChange: true)
-	sendEvent(name: "color", value: colorUtil.hslToHex(hue/3.6 as int, saturation as int))
+    def status = state.on_off
+    if (status == 1) {
+        status = "on"
+    } else {
+        status = "off"
+        state = state.dft_on_state
+    }
+    def mode = state.mode
+    def level = state.brightness
+    def color_temp = state.color_temp
+    def hue = state.hue
+    def saturation = state.saturation
+    log.info "$device.name $device.label: Power: ${status} / Mode: ${mode} / Brightness: ${level}% / Color Temp: ${color_temp}K / Hue: ${hue} / Saturation: ${saturation}"
+    sendEvent(name: "switch", value: status, isStateChange: true)
+    sendEvent(name: "bulbMode", value: mode, isStateChange: true)
+    sendEvent(name: "level", value: level, isStateChange: true)
+    sendEvent(name: "colorTemperature", value: color_temp, isStateChange: true)
+    sendEvent(name: "hue", value: hue, isStateChange: true)
+    sendEvent(name: "saturation", value: saturation, isStateChange: true)
+    sendEvent(name: "color", value: colorUtil.hslToHex(hue/3.6 as int, saturation as int))
+}
+
+//    ----- Send the Command to the Bridge -------------------------
+private sendCmdtoServer(command, action){
+    def headers = [:] 
+    headers.put("HOST", "$gatewayIP:8082")   // Same as on Hub.
+    headers.put("tplink-iot-ip", deviceIP)
+    headers.put("tplink-command", command)
+    headers.put("command", "deviceCommand")
+    sendHubCommand(new physicalgraph.device.HubAction([
+        headers: headers],
+        device.deviceNetworkId,
+        [callback: action]
+    ))
 }
