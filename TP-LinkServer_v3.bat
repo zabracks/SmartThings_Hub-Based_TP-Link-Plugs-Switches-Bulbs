18ac7d39f75ca5fce4ff9cf2365a602aea54cf71
DaveGut <djgutheinz@outlook.com>
2017-07-14 10:07:08 -0500

    Add files via upload

--- TP-LinkServer_v3.bat
+++ TP-LinkServer_v3.bat
@@ -2,7 +2,7 @@ REM  Place this file in the TP-Link Bulb top level directory.
 REM  Add path to the bridge directory if auto-starting.
 //cd c:\1-TP Link\
 color 3f
-title TP-Link Hub Java Script
+title TP-Link Hub Java Script Console
 prompt $_
 Echo off
 CLS
